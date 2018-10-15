8ff382ed8660ee72d6b1694e5f5b90130d612d81
Carlos Fuentes <thein300000@gmail.com>
2018-10-15 00:15:29 -0500

    Seemingly finished initial setup

--- gainspend/views.py
+++ gainspend/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from .models import TableMovements
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def main_dashboard(request):
     movements = TableMovements.objects.all()
     context = {
@@ -10,6 +12,7 @@ def main_dashboard(request):
     }
     return render(request, 'gainspend/pages/main_dashboard.html', context)
 
+@login_required
 def movement_data(request):
     movements = TableMovements.objects.all()
     json = serializers.serialize('json', movements)
