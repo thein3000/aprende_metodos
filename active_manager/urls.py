2a5563be994908e0529f9ec76c86ceb4db0f4292
Carlos Fuentes <thein300000@gmail.com>
2018-10-27 18:54:27 -0500

    Modified app urls

--- active_manager/urls.py
+++ active_manager/urls.py
@@ -18,6 +18,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-    path('gainspend/', include('gainspend.urls')),
+    path('', include('gainspend.urls')),
     path('admin/', admin.site.urls),
 ]
