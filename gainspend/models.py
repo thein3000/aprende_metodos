5054bbf646ede68678cbc36f62af825e69caba0b
Carlos Fuentes <thein300000@gmail.com>
2018-11-11 11:39:02 -0600

    Completely chanee project

--- gainspend/models.py
+++ gainspend/models.py
@@ -1,48 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class TableAccounts(models.Model):
-    field_id = models.IntegerField(db_column='_id', primary_key=True, blank=True)  # Field renamed because it started with '_'.
-    account = models.TextField(blank=True, null=True)
-    icon = models.TextField(blank=True, null=True)
+class Method(models.Model):
+    field_id = models.IntegerField(db_column='_id', primary_key=True, blank=True)
+    name = models.TextField(blank=True, null=True)
+    is_subject = models.BooleanField(blank=True, null=True)
 
-    @property
-    def account_capital(self):
-        movements = TableMovements.objects.filter(account=self.account)
-        account_capital = 0
-        for movement in movements:
-            if movement.sign == '+':
-                account_capital = account_capital + movement.amount
-            else:
-                account_capital = account_capital - movement.amount
-
-        return account_capital
-
-    class Meta:
-        managed = False
-        db_table = 'table_accounts'
-
-
-class TableCategories(models.Model):
-    field_id = models.IntegerField(db_column='_id', primary_key=True, blank=True)  # Field renamed because it started with '_'.
-    category = models.TextField(blank=True, null=True)
-    icon = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'table_categories'
-
-
-class TableMovements(models.Model):
-    field_id = models.IntegerField(db_column='_id', primary_key=True, blank=True)  # Field renamed because it started with '_'.
-    account = models.TextField(blank=True, null=True)
-    category = models.TextField(blank=True, null=True)
-    amount = models.FloatField(blank=True, null=True)
-    sign = models.TextField(blank=True, null=True)
-    detail = models.TextField(blank=True, null=True)
-    date = models.DateField(blank=True, null=True)
-    transfer = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'table_movements'
+class UserMethod(models.Model):
+    field_id = models.IntegerField(db_column='_id', primary_key=True, blank=True)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete = models.CASCADE)
+    method = models.ForeignKey(Method, null=True, blank=True, on_delete = models.CASCADE)
+    completed = models.BooleanField(blank=True, null=True)
+    paramaters = models.TextField(blank=True, null=True)
--- gainspend/urls.py
+++ gainspend/urls.py
@@ -5,23 +5,23 @@ from . import views
 urlpatterns = [
     # ------------------------------ MOVEMENT -----------------------------------
     path('',views.main_dashboard, name='main_dashboard'),
-    path('movement_data/',views.movement_data, name='movement_data'),
-    path('movement_detail/<int:field_id>/',views.movement_detail, name='movement_detail'),
-    path('movement_detail/movement_update/<int:field_id>/',views.movement_update, name='movement_update'),
-    path('movement_detail/movement_delete/<int:field_id>/',views.movement_delete, name='movement_delete'),
-    path('register_income/',views.register_income, name='register_income'),
-    path('register_expense/',views.register_expense, name='register_expense'),
-    path('register_transfer/',views.register_transfer, name='register_transfer'),
-    # ------------------------------ ACCOUNT -----------------------------------
-    path('account_data/',views.account_data, name='account_data'),
-    path('account_catalogue/',views.account_catalogue, name='account_catalogue'),
-    path('account_catalogue/<int:field_id>/',views.account_detail,name='account_detail'),
-    path('account_catalogue/account_register/',views.account_register, name='account_register'),
-    path('account_catalogue/account_delete/<int:field_id>/',views.account_delete, name='account_delete'),
-    path('movement_data/<str:account>/',views.account_movement_data, name='account_movement_data'),
-    # ------------------------------ CATEGORY -----------------------------------
-    path('category_data/',views.category_data, name='category_data'),
-    path('category_catalogue/',views.category_catalogue, name='category_catalogue'),
-    path('category_catalogue/category_register/',views.category_register, name='category_register'),
-    path('category_catalogue/category_delete/<int:field_id>/',views.category_delete, name='category_delete'),
+    # path('movement_data/',views.movement_data, name='movement_data'),
+    # path('movement_detail/<int:field_id>/',views.movement_detail, name='movement_detail'),
+    # path('movement_detail/movement_update/<int:field_id>/',views.movement_update, name='movement_update'),
+    # path('movement_detail/movement_delete/<int:field_id>/',views.movement_delete, name='movement_delete'),
+    # path('register_income/',views.register_income, name='register_income'),
+    # path('register_expense/',views.register_expense, name='register_expense'),
+    # path('register_transfer/',views.register_transfer, name='register_transfer'),
+    # # ------------------------------ ACCOUNT -----------------------------------
+    # path('account_data/',views.account_data, name='account_data'),
+    # path('account_catalogue/',views.account_catalogue, name='account_catalogue'),
+    # path('account_catalogue/<int:field_id>/',views.account_detail,name='account_detail'),
+    # path('account_catalogue/account_register/',views.account_register, name='account_register'),
+    # path('account_catalogue/account_delete/<int:field_id>/',views.account_delete, name='account_delete'),
+    # path('movement_data/<str:account>/',views.account_movement_data, name='account_movement_data'),
+    # # ------------------------------ CATEGORY -----------------------------------
+    # path('category_data/',views.category_data, name='category_data'),
+    # path('category_catalogue/',views.category_catalogue, name='category_catalogue'),
+    # path('category_catalogue/category_register/',views.category_register, name='category_register'),
+    # path('category_catalogue/category_delete/<int:field_id>/',views.category_delete, name='category_delete'),
 ]
