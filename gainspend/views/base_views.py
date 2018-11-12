aebbb1b1db331a3a677861f812916d5e38cc8765
Carlos Fuentes <thein300000@gmail.com>
2018-11-11 22:31:18 -0600

    Added main dashboard responsiveness for completed subjects and methods

--- gainspend/views/base_views.py
+++ gainspend/views/base_views.py
@@ -13,8 +13,18 @@ from gainspend.forms import UserMethodForm
 @login_required
 def main_dashboard(request):
     methods = Method.objects.all()
+    completed_list = []
+    for method in methods:
+        completed = len(UserMethod.objects.filter(user=request.user, method=method))
+        last_completed_value = completed
+        if completed >= 1:
+            completed_list.append(True)
+        else:
+            completed_list.append(False)
+    completed_list.insert(0,True)
+    methods_list = zip(methods, completed_list)
     context = {
-        "methods": methods
+        "methods_list": methods_list
     }
     return render(request, 'gainspend/pages/main_dashboard.html', context)
 
@@ -42,6 +52,20 @@ def register_method_completion(request):
                 seconds = post_seconds
             )
             user_method.save()
+            # If next method is a subject, then mark it as completed to enable it in main_dashboard
+            try:
+                next_method = Method.objects.get(field_id = (post_method.field_id + 1))
+            except Exception as e:
+                print(e)
+                return redirect("successful_excercise")
+            if next_method.is_subject:
+                next_user_method = UserMethod(
+                    user = request.user,
+                    method = next_method,
+                    completed = True,
+                    seconds = 0
+                )
+                next_user_method.save()
             return redirect("successful_excercise")
         else:
             return redirect("main_dashboard")
