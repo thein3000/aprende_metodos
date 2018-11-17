27918977b1939cef4a5d82c7df1e206a7abc7273
Carlos Fuentes <thein300000@gmail.com>
2018-11-17 10:52:52 -0600

    Added support for Cubica

--- gainspend/views/minimos_cuadrados.py
+++ gainspend/views/minimos_cuadrados.py
@@ -58,7 +58,7 @@ def preface_cuadratica(request):
     context = {
         "method": method
     }
-    return render(request, 'gainspend/pages/preface_linea_recta.html', context)
+    return render(request, 'gainspend/pages/preface_cuadratica.html', context)
 
 @login_required
 def excercise_cuadratica(request):
@@ -93,6 +93,51 @@ def excercise_cuadratica(request):
         "method": method
     }
     return render(request, 'gainspend/pages/excercise_cuadratica.html', context)
+
+@login_required
+def preface_cubica(request):
+    method = Method.objects.filter(name="Cúbica").first()
+    context = {
+        "method": method
+    }
+    return render(request, 'gainspend/pages/preface_cubica.html', context)
+
+@login_required
+def excercise_cubica(request):
+    method = Method.objects.filter(name="Cúbica").first()
+    # Variables del problema
+    x = [ 1.1, 1.9, 2.4, 4.8, 5.1,10.5]
+    y = [ 2.5, 2.7, 3.7, 5.2, 6.0, 8.3]
+    mode = 3
+    lista_coeficientes = minimos_cuadrados_general.metodo_minimos_cuadrados(mode,x,y)
+    print(f'Resultado minimos: {lista_coeficientes}')
+    # Variables de presentacion del problema
+    x_print = [ 1.1, 1.9, 2.4, 4.8, 5.1,10.5]
+    y_print = [ 2.5, 2.7, 3.7, 5.2, 6.0, 8.3]
+    xy_print = zip(x_print,y_print)
+    if type(lista_coeficientes) != type("string"):
+        result_a0 = lista_coeficientes[0]
+        result_a1 = lista_coeficientes[1]
+        result_a2 = lista_coeficientes[2]
+        result_a3 = lista_coeficientes[3]
+    else:
+        result_a0 = lista_coeficientes
+        result_a1 = lista_coeficientes
+        result_a2 = lista_coeficientes
+        result_a3 = lista_coeficientes
+    # Datos del formulario
+    data = {'method_id': method.field_id}
+    user_method_form = UserMethodForm(initial=data)
+    context = {
+        "user_method_form": user_method_form,
+        "xy_print": xy_print,
+        "result_a0": result_a0,
+        "result_a1": result_a1,
+        "result_a2": result_a2,
+        "result_a3": result_a3,
+        "method": method
+    }
+    return render(request, 'gainspend/pages/excercise_cubica.html', context)
        # # "Minimos Cuadrados"
        # "Linea Recta"
        # "Cuadratica"
