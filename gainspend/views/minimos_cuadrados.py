374bb027c50455540731e505fa6b94b1ebde2e3a
Carlos Fuentes <thein300000@gmail.com>
2018-11-17 10:31:04 -0600

    Added support for Cuadratica

--- gainspend/views/minimos_cuadrados.py
+++ gainspend/views/minimos_cuadrados.py
@@ -50,6 +50,49 @@ def excercise_linea_recta(request):
         "method": method
     }
     return render(request, 'gainspend/pages/excercise_linea_recta.html', context)
+
+
+@login_required
+def preface_cuadratica(request):
+    method = Method.objects.filter(name="Cuadratica").first()
+    context = {
+        "method": method
+    }
+    return render(request, 'gainspend/pages/preface_linea_recta.html', context)
+
+@login_required
+def excercise_cuadratica(request):
+    method = Method.objects.filter(name="Cuadratica").first()
+    # Variables del problema
+    x = [ 1.1, 1.9, 2.4, 4.8, 5.1,10.5]
+    y = [ 2.5, 2.7, 3.7, 5.2, 6.0, 8.3]
+    mode = 2
+    lista_coeficientes = minimos_cuadrados_general.metodo_minimos_cuadrados(mode,x,y)
+    print(f'Resultado minimos: {lista_coeficientes}')
+    # Variables de presentacion del problema
+    x_print = [ 1.1, 1.9, 2.4, 4.8, 5.1,10.5]
+    y_print = [ 2.5, 2.7, 3.7, 5.2, 6.0, 8.3]
+    xy_print = zip(x_print,y_print)
+    if type(lista_coeficientes) != type("string"):
+        result_a0 = lista_coeficientes[0]
+        result_a1 = lista_coeficientes[1]
+        result_a2 = lista_coeficientes[2]
+    else:
+        result_a0 = lista_coeficientes
+        result_a1 = lista_coeficientes
+        result_a2 = lista_coeficientes
+    # Datos del formulario
+    data = {'method_id': method.field_id}
+    user_method_form = UserMethodForm(initial=data)
+    context = {
+        "user_method_form": user_method_form,
+        "xy_print": xy_print,
+        "result_a0": result_a0,
+        "result_a1": result_a1,
+        "result_a2": result_a2,
+        "method": method
+    }
+    return render(request, 'gainspend/pages/excercise_cuadratica.html', context)
        # # "Minimos Cuadrados"
        # "Linea Recta"
        # "Cuadratica"
