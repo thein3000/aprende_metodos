08faa9abca5f7e77df243e7ea7c903eb268ec02a
Carlos Fuentes <thein300000@gmail.com>
2018-11-19 13:29:26 -0600

    Added support for Regla Trapezoidal

--- gainspend/views/interpolacion.py
+++ gainspend/views/interpolacion.py
@@ -7,6 +7,7 @@ from gainspend.metodos_numericos import newton_hacia_adelante
 from gainspend.metodos_numericos import newton_hacia_atras
 from gainspend.metodos_numericos import lagrange
 from gainspend.metodos_numericos import interpolacion_lineal
+from gainspend.metodos_numericos import newton_con_diferencias_divididas
 # from gainspend.randomizers import
 
 @login_required
@@ -144,6 +145,40 @@ def excercise_lagrange(request):
         "method": method
     }
     return render(request, 'gainspend/pages/excercise_lagrange.html', context)
+
+@login_required
+def preface_newton_con_diferencias_divididas(request):
+    method = Method.objects.filter(name="Newton con diferencias divididas").first()
+
+    context = {
+        "method": method
+    }
+    return render(request, 'gainspend/pages/preface_newton_con_diferencias_divididas.html', context)
+
+@login_required
+def excercise_newton_con_diferencias_divididas(request):
+    method = Method.objects.filter(name="Newton con diferencias divididas").first()
+    # Variables del problema
+    t = 7
+    x = [7.3,6.5,6.1,5.5]
+    y = [-.28,-1.35,-1.96,-2.74]
+    result = newton_con_diferencias_divididas.metodo_newton_con_diferencias_divididas(x,y,t)
+    # Variables de presentacion del problema
+    t_print = 7
+    x_print = [7.3,6.5,6.1,5.5]
+    y_print = [-.28,-1.35,-1.96,-2.74]
+    xy_print = zip(x_print,y_print)
+    # Datos del formulario
+    data = {'method_id': method.field_id}
+    user_method_form = UserMethodForm(initial=data)
+    context = {
+        "user_method_form":user_method_form,
+        "xy_print": xy_print,
+        "t_print": t_print,
+        "result": result,
+        "method": method
+    }
+    return render(request, 'gainspend/pages/excercise_newton_con_diferencias_divididas.html', context)
 
        # # "Interpolación",
        # "Newton hacia adelante"
