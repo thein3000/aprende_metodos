48c49aea4b9c8bd19c4f5765592fd1c11393761a
Carlos Fuentes <thein300000@gmail.com>
2018-11-23 03:06:10 -0600

    Added whitenoise again and stic changes

--- active_manager/settings.py
+++ active_manager/settings.py
@@ -127,4 +127,5 @@ LOGOUT_REDIRECT_URL = '/accounts/login'
 # All settings common to all environments
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
